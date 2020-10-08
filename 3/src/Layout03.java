08b193a918b603ef35ffca292f91eb7e85efe3ae
Kaoru Ishizawa <36197417+10kaoru12@users.noreply.github.com>
2020-10-08 21:32:48 +0900

    第3回の授業課題

---
+++ 3/src/Layout03.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+
+public class Layout03 extends JFrame {
+
+    public static void main(String[] args) {
+        Layout03 w = new Layout03( "Layout03" );
+        w.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        w.setSize( 250, 300 );
+        w.setVisible( true );
+    }
+    public Layout03( String title ){
+        super( title );
+        JPanel pane = (JPanel)getContentPane();
+        pane.setLayout( new GridLayout( 4, 3 ) ); // レイアウト方法を指定 4x3
+
+        for( int i=0 ; i<10 ; i++ ){
+            pane.add( new JButton( Integer.toString(i) ) ); // ボタンの生成と追加
+        }
+    }
+}
